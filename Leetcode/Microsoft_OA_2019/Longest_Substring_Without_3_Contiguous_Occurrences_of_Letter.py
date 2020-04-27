37d0f84612136407b29c520bed279e27ad115305
Deepankar <deepankar.singh@diginex.com>
2020-04-27 13:51:56 +0200

    Longest_Substring_Without_3_Contiguous_Occurrences

---
+++ Leetcode/Microsoft_OA_2019/Longest_Substring_Without_3_Contiguous_Occurrences_of_Letter.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/discuss/interview-question/398031/
+"""
+Discription of question in above link
+"""
+from collections import defaultdict
+from typing import Dict, List
+
+
+def validLongestSubstring(input: str) -> str:
+    start: int = 0
+    str_start: int = 0
+    end: int = 1
+    max_length: int = 1
+    max_start: int = 0
+    first: str = input[0]
+    cnt: int = 1
+    flag: int = 1
+    while(end < len(input)):
+        if input[end] == first and cnt >= 2:
+            flag = 0
+            if(max_length < end - str_start):
+                max_start = str_start
+                max_length = end-str_start
+            str_start = end-1
+            start = end-1
+            first = input[start]
+            cnt = 1
+        elif input[end] != first:
+            start = end
+            first= input[start]
+            end +=1
+            cnt = 1
+        else:
+            cnt +=1
+            end +=1
+    if flag ==1: 
+        return input  
+    return input[max_start: max_start+max_length]
+
+# Driver program to test the above function
+def main(): 
+    print(validLongestSubstring("aabbaaaaabb")) # expected: aabbaa
+    print(validLongestSubstring("aabbaabbaabbaa"))  # expected: aabbaabbaabbaa
+    print(validLongestSubstring("abbaabbaaabbaaa"))  # expected: abbaabbaa
+    
+if __name__=="__main__": 
+    main()
