3a8df0d3aac09af795b93ad33641dee5410da4f1
Deepankar <deepankar.singh@diginex.com>
2020-05-06 12:20:51 +0200

    997_Find_The_Town_Judge

---
+++ Leetcode/Algorithms_Questions/997_Find_The_Town_Judge.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/surrounded-regions/
+"""
+Discription of question in above link
+"""
+
+
+def findJudge(N, trust):
+    count = [0 for i in range(0, N+1)]
+    for item in trust:
+        count[item[0]] -= 1
+        count[item[1]] += 1
+    for i in range(1, N+1):
+        if(count[i] == N-1):
+            return i
+    return -1
+
+
+def main():
+    trust = [[1, 2]]
+    N = 2
+    print(findJudge(N, trust))
+
+
+if __name__ == "__main__":
+    main()
