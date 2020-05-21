2992821900ed7ba1b715391dd6d37fffb7988110
Deepankar <deepankar080892@gmail.com>
2020-05-21 15:30:19 +0200

    Partition_Labels

---
+++ Leetcode/Amazon_OA/Partition_Labels.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/partition-labels/
+"""
+Discription of question in above link
+"""
+
+from typing import List
+import operator
+
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        last_occur = { item: index for index, item in enumerate(S)} 
+        
+        flag, current, result = 0 , 0, []
+        for index, item  in enumerate(S):
+            current = max(current, last_occur[item])
+            if(current == index):
+                result.append(current - flag +1)
+                flag = index + 1
+        
+        return result
+        
+        
+def main():
+    S = "ababcbacadefegdehijhklij"
+    sol = Solution()
+    print(sol.partitionLabels(S))
+
+
+if __name__ == "__main__":
+    main()
