bea66ac0531d8f32b9c7a8121d2bf9af0afb430c
Deepankar <deepankar080892@gmail.com>
2020-05-21 13:47:53 +0200

    Reorder_Data_in_Log_Files

---
+++ Leetcode/Amazon_OA/Reorder_Data_in_Log_Files.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/reorder-data-in-log-files/
+"""
+Discription of question in above link
+"""
+
+from typing import List
+import operator
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        result = []
+        letterLog = []
+        digitLog = []
+        
+        for log in logs:
+            splitLog =  log.split(" ")
+            if splitLog[1].isdigit():
+                digitLog.append(log)
+            else:
+                letterLog.append([splitLog[0], " ".join(splitLog[1:])])
+        
+        for i in sorted(letterLog, key=operator.itemgetter(1,0)):
+            result.append(" ".join(i))
+        return result + digitLog      
+        
+        
+def main():
+    logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+    sol = Solution()
+    print(sol.reorderLogFiles(logs))
+
+
+if __name__ == "__main__":
+    main()
