a3c7eb9c4aad69f5bc14419c19345e034799c8e1
Deepankar <deepankar080892@gmail.com>
2021-07-02 20:49:21 +0530

    using greedy approach

--- Greedy_Algorithm/minimum_number_of_platforms_required_for_a_railway_bus_station.py
+++ Greedy_Algorithm/minimum_number_of_platforms_required_for_a_railway_bus_station.py
@@ -1,18 +1,47 @@
+# def findPlatform(arr, dep, n):
+#   plat_needed = 1
+#   result = 1
+#   i, j = 0, 0
+
+#   for i in range(n):
+#     plat_needed = 1
+
+#     for j in range(i + 1, n):
+#       if ((arr[i] >= arr[j] and arr[i] <= dep[j]) or (arr[j] >= arr[i] and arr[j] <= dep[i])):
+#           plat_needed += 1
+#       result = max(result, plat_needed);
+
+#   return result
+
 def findPlatform(arr, dep, n):
-  plat_needed = 1
-  result = 1
-  i, j = 0, 0
 
-  for i in range(n):
+    arr.sort()
+    dep.sort()
+
     plat_needed = 1
+    result = 1
+    
+    i = 1
+    j = 0
 
-    for j in range(i + 1, n):
-      if ((arr[i] >= arr[j] and arr[i] <= dep[j]) or (arr[j] >= arr[i] and arr[j] <= dep[i])):
-          plat_needed += 1
-      result = max(result, plat_needed);
+    while (i < n and j < n):
 
-  return result
+        if (arr[i] <= dep[j]):
+ 
+            plat_needed += 1
+            i += 1
 
+        elif (arr[i] > dep[j]):
+ 
+            plat_needed -= 1
+            j += 1
+ 
+        if (plat_needed > result):
+            result = plat_needed
+ 
+    return result
+ 
+# Driver code
 arr = [900, 940, 950, 1100, 1500, 1800]
 dep = [910, 1200, 1120, 1130, 1900, 2000]
 n = len(arr)
